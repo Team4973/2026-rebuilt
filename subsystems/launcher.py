f279a20a3453fe8dfc6edcb6a40186f1dd02548c
Cosimo Aho <cosimoaho@gmail.com>
2026-03-13 15:16:42 -0700

    Update launcher.py

--- subsystems/launcher.py
+++ subsystems/launcher.py
@@ -8,17 +8,14 @@ class Launcher(Subsystem):
     def __init__(self, launcher_motor_id: int = 33):
         super().__init__()
         self._launcher_motor = hardware.TalonFX(launcher_motor_id)
-        self._feeder_motor = hardware.TalonFX(32)
         self._duty_cycle = controls.DutyCycleOut(0)
 
     def set_speed(self, speed: float) -> None:
         """Set motor speed (-1.0 to 1.0)."""
         self._launcher_motor.set_control(self._duty_cycle.with_output(speed))
-        self._feeder_motor.set_control(self._duty_cycle.with_output(-speed))
-
     def stop(self) -> None:
         """Stop the motor."""
         self._launcher_motor.set_control(self._duty_cycle.with_output(0))
-        self._feeder_motor.set_control(self._duty_cycle.with_output(0))
+       
 
     #PID - def set_rpm(self) -> None:
